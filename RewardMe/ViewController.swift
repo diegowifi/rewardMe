6ae6b5249c62d74104c0d4850e76975735e04c9b
alumno <alumno@Mac-16.local>
2016-11-02 12:58:42 -0600

    Initial commit

---
+++ RewardMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RewardMe
+//
+//  Created by alumno on 02/11/16.
+//  Copyright © 2016 alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
